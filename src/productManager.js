e1c93eb11fc913c883a6377437ee3ae839dddcb1
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-09-29 18:25:13 -0300

    Update productManager.js

--- src/productManager.js
+++ src/productManager.js
@@ -1,61 +1,30 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs').promises;
 
 class ProductManager {
-  constructor(filePath) {
-    this.path = filePath;
+  constructor(productsFilePath, cartsFilePath) {
+    this.productsFilePath = productsFilePath;
+    this.cartsFilePath = cartsFilePath;
   }
 
-  async addProduct(product) {
-    const products = await this.getProducts();
-    const newProduct = {
-      ...product,
-      id: products.length + 1,
-    };
-    products.push(newProduct);
-    await this.saveProducts(products);
-    return newProduct;
-  }
-
-  async getProducts() {
+  async getPlantProducts() {
     try {
-      const data = await fs.promises.readFile(this.path, 'utf-8');
-      return JSON.parse(data);
+      const data = await fs.readFile(this.productsFilePath, 'utf8');
+      const products = JSON.parse(data);
+      return products.slice(0, 4);
     } catch (error) {
-      return [];
+      throw error;
     }
   }
 
-  async getProductById(id) {
-    const products = await this.getProducts();
-    return products.find((product) => product.id === id);
-  }
-
-  async updateProduct(id, updatedProduct) {
-    const products = await this.getProducts();
-    const index = products.findIndex((product) => product.id === id);
-    if (index !== -1) {
-      products[index] = {
-        ...updatedProduct,
-        id: id, // Ensure the ID remains the same
-      };
-      await this.saveProducts(products);
-      return products[index];
+  async getPlantProductById(productId) {
+    try {
+      const data = await fs.readFile(this.productsFilePath, 'utf8');
+      const products = JSON.parse(data);
+      const plantProduct = products.find((product) => product.id === parseInt(productId));
+      return plantProduct;
+    } catch (error) {
+      throw error;
     }
-    return null;
-  }
-
-  async deleteProduct(id) {
-    const products = await this.getProducts();
-    const index = products.findIndex((product) => product.id === id);
-    if (index !== -1) {
-      products.splice(index, 1);
-      await this.saveProducts(products);
-    }
-  }
-
-  async saveProducts(products) {
-    await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
   }
 }
 
