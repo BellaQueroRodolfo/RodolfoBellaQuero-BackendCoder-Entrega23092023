588febac7e278707847d0a7abe76ed0870078316
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-09-29 18:15:07 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-const ProductManager = require('./productManager');
-const productManager = new ProductManager('./data/products.json', './data/carts.json');
-app.use(express.json());
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
 
+app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/carts', cartsRouter);
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
